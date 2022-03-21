15aaeb9309494f0d323e3561bd7d92ba89f9c976
dhailiz <zdhaili3@gmail.com>
2022-03-21 15:59:04 +0100

    js verbesert und emoji video eingefügt

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ document.getElementById("defaultOpen").click();
 function tabOpener(evt, kat) {
     var i, tabcontent, tablinks;
 
-    document.getElementsByClassName("tabcontent");
+    tabcontent= document.getElementsByClassName("tabcontent");
     for (i = 0; i < tabcontent.length; i++) {
       tabcontent[i].style.display = "none";
     }
@@ -12,6 +12,6 @@ function tabOpener(evt, kat) {
     tablinks[i].className = tablinks[i].className.replace(" active", "");
   }
 
-  document.getElementById(kat).style.display = "block";
+  document.getElementById(kat).style.display = "flex";
   evt.currentTarget.className += " active";
 }
