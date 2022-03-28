7ca707a767595848b9ac30100f81aa6b8e8840e6
dhailiz <zdhaili3@gmail.com>
2022-03-28 02:23:21 +0200

    JS Modal hinzugefügt

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ function tabOpener(evt, kat) {
   document.getElementById(kat).style.display = "flex";
   evt.currentTarget.className += " active";
 }
+
+
+
+
+//Modal
+var modal = document.getElementById("myModal");
+
+var btn = document.getElementById("myBtn")
+
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
