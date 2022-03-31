e3444a1639fc3c6d2fca2cf75f409336efcf32c2
dhailiz <zdhaili3@gmail.com>
2022-03-31 15:15:59 +0200

    modal bug

--- script.js
+++ script.js
@@ -20,22 +20,38 @@ function tabOpener(evt, kat) {
 
 
 //Modal
-var modal = document.getElementById("myModal");
+// Get the button that opens the modal
+var btn = document.querySelectorAll("button.modal-button");
 
-var btn = document.getElementById("myBtn")
+// All page modals
+var modals = document.querySelectorAll('.modal');
 
-var span = document.getElementsByClassName("close")[0];
+// Get the <span> element that closes the modal
+var spans = document.getElementsByClassName("close");
 
-btn.onclick = function() {
-  modal.style.display = "block";
+// When the user clicks the button, open the modal
+for (var i = 0; i < btn.length; i++) {
+ btn[i].onclick = function(e) {
+    e.preventDefault();
+    modal = document.querySelector(e.target.getAttribute("href"));
+    modal.style.display = "block";
+ }
 }
 
-span.onclick = function() {
-  modal.style.display = "none";
+// When the user clicks on <span> (x), close the modal
+for (var i = 0; i < spans.length; i++) {
+ spans[i].onclick = function() {
+    for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+    }
+ }
 }
 
+// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
+    if (event.target.classList.contains('modal')) {
+     for (var index in modals) {
+      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+     }
+    }
 }
